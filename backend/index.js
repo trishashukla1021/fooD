46a397fc7a8408f935387cc912f429b80619282e
Trisha Shukla <trishashukla1021@gmail.com>
2024-06-20 15:40:45 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT|5000
 const mongoDB= require("./db")
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
